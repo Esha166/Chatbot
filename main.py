00057956c4ec6c529fd73075564ff3a5140c85d2
Esha <eshacf4@gmail.com>
2025-07-11 18:14:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 
 load_dotenv()
 API_KEY = os.getenv("GEMINI_API_KEY")
-chat = []
+
 def get_gemini_response(prompt):
     url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={API_KEY}"
     headers = {"Content-Type": "application/json"}
@@ -23,12 +23,3 @@ reply = get_gemini_response(user_input)
 if user_input != "":
         st.write("You:", user_input)
         st.write("Bot:", reply)
-chat.append({"role": "user", "content": user_input})
-chat.append({"role": "assistant", "content": reply})
-
-for message in chat:
-    if message['role'] == "user":
-        print(message['content'])
-        st.write("You:", message['content'])
-    else:
-        st.write("Bot:", message['content'])
